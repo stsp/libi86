86669bb7174982b7c580d47cc3224babcb26a8e3
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2021-06-04 09:15:46 +0000

    <dos.h>: _dos_findfirst, _dos_findnext, & _dos_findclose for ACK

---
+++ host-ack/libi86/internal/dos.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2021 TK Chia
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the developer(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LIBI86_LIBI86_INTERNAL_DOS_H_
+#define _LIBI86_LIBI86_INTERNAL_DOS_H_
+
+#ifndef _LIBI86_COMPILING_
+# error "<libi86/internal/dos.h> should only be used when compiling libi86!"
+#endif
+
+#include <stdbool.h>
+#include <i86.h>
+#include <libi86/internal/cdefs.h>
+
+_LIBI86_BEGIN_EXTERN_C
+
+static bool
+__libi86_msdos_drive_letter_p (char c)
+{
+  switch (c)
+    {
+    case 'A':  case 'B':  case 'C':  case 'D':  case 'E':  case 'F':  case 'G':
+    case 'H':  case 'I':  case 'J':  case 'K':  case 'L':  case 'M':  case 'N':
+    case 'O':  case 'P':  case 'Q':  case 'R':  case 'S':  case 'T':  case 'U':
+    case 'V':  case 'W':  case 'X':  case 'Y':  case 'Z':
+    case 'a':  case 'b':  case 'c':  case 'd':  case 'e':  case 'f':  case 'g':
+    case 'h':  case 'i':  case 'j':  case 'k':  case 'l':  case 'm':  case 'n':
+    case 'o':  case 'p':  case 'q':  case 'r':  case 's':  case 't':  case 'u':
+    case 'v':  case 'w':  case 'x':  case 'y':  case 'z':
+      return true;
+    default:
+      return false;
+    }
+}
+
+static bool
+__libi86_msdos_path_sep_p (char c)
+{
+  switch (c)
+    {
+    case '/':
+    case '\\':
+      return true;
+    default:
+      return false;
+    }
+}
+
+extern void __libi86_msdos_set_dta (void *new_dta);
+extern unsigned __libi86_msdos_do_findfirst (const char *path, unsigned attr);
+extern unsigned __libi86_msdos_do_next (void);
+
+#endif
