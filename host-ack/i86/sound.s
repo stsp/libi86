5c6b76ed9809b869dd78b1d0e704d67468a66938
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2021-10-30 18:24:05 +0000

    <i86.h>: add sound (.) & nosound () for ACK (take 2)

---
+++ host-ack/i86/sound.s
@@ -0,0 +1,60 @@
+#
+/*
+ * Copyright (c) 2018--2021 TK Chia
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the developer(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "libi86/internal/sect.h"
+
+	.define	_sound
+_sound:
+	mov	bx, sp
+	mov	cx, 2(bx)
+	mov	ax, 0x34dd		/* 1193181 == 0x1234dd */
+	cwd
+	cmp	cx, 19
+	jb	.play			/* dx == 0 */
+	mov	bx, cx
+	shr	bx, 1
+	add	ax, bx
+	adc	dx, 0x12
+	div	cx			/* this will never overflow */
+	xchg	dx, ax
+.play:
+	pushf
+	cli
+	inb	0x61
+	orb	al, 0x03
+	outb	0x61
+	movb	al, 0xb6
+	outb	0x43
+	xchg	dx, ax
+	outb	0x42
+	movb	al, ah
+	outb	0x42
+	popf
+	ret
