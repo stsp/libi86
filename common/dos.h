ed2ed4b7fc2d4ef325a1d67fc71f96e3013d090c
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2022-01-08 07:58:16 +0000

    <dos.h>: add _dos_settime (.)

--- common/dos.h
+++ common/dos.h
@@ -124,6 +124,7 @@ extern unsigned _dos_setdate (const struct dosdate_t *__date);
 extern void _dos_setdrive (unsigned __drive, unsigned *__total);
 extern unsigned _dos_setfileattr (const char *__path, unsigned __attributes);
 extern unsigned _dos_setftime (int __handle, unsigned __date, unsigned __time);
+extern unsigned _dos_settime (const struct dostime_t *__time);
 extern unsigned _dos_write (int __handle, __libi86_fpcv_t __buf,
 			    unsigned __count, unsigned *__bytes);
 /*
