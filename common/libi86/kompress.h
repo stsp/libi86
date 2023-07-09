8cdfca3e5250f8a0caad1dda32772d40eecd7556
tkchia <tkchia@users.noreply.github.com>
2023-07-09 18:43:57 +0000

    Add new _lz4cpy (...) routine & new <libi86/kompress.h> header

---
+++ common/libi86/kompress.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 TK Chia
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the developer(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LIBI86_LIBI86_KOMPRESS_H_
+#define _LIBI86_LIBI86_KOMPRESS_H_
+
+#include <libi86/internal/cdefs.h>
+
+_LIBI86_BEGIN_EXTERN_C
+
+extern __libi86_fpv_t _lz4cpy (__libi86_fpv_t __dest,
+			       __libi86_fpcv_t __blk_src,
+			       __libi86_size_t __blk_sz);
+
+#ifndef _LIBI86_COMPILING_
+# ifndef __STRICT_ANSI__
+_LIBI86_REDIRECT_3 (__libi86_fpv_t, lz4cpy, __libi86_fpv_t, __libi86_fpcv_t,
+					    __libi86_size_t, _lz4cpy)
+# endif
+#endif
+
+_LIBI86_END_EXTERN_C
+
+#endif
